582b31ba4229d8aa42c4a30e686a59972f0cf137
TFonta <tomaso.fontanini@gmail.com>
2023-03-28 17:14:14 +0200

    spring cleaning 3

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
-python ./test.py --name celeba_model --cross_att_all_layers --single_layer_mask_enc \
-    --no_self_last_layers --exclude_bg --multi_scale_style_enc  \
+python ./test.py --name celeba_model --exclude_bg \
     --sample_dir ./results/train_celeba/ --checkpoints_dir ./checkpoints/train_celeba/ \
     --batchSize 16 --crop_size 256 --style_dim 256 --load_size 256 --dataset_mode custom \
     --label_dir ./datasets/CelebA-HQ/train/labels \
