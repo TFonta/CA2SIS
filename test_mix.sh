8e59b6f571400dee5a1c9c95545cd3acc52f2e7e
Tomaso FONTANINI <tomaso.fontanini@ui01.hpc.unipr.it>
2024-04-17 13:37:08 +0200

    fixed args

--- test_mix.sh
+++ test_mix.sh
@@ -5,5 +5,5 @@ python ./test_style_mix.py --name celeba_model --exclude_bg \
     --image_dir ./datasets/CelebA-HQ/train/images \
     --label_dir_test ./datasets/CelebA-HQ/test/labels \
     --image_dir_test ./datasets/CelebA-HQ/test/images \
-     --label_nc 19 --no_instance --nThreads 4 --gpu_ids 0 --part_to_swap makeup --swap_type style
+     --label_nc 19 --no_instance --nThreads 4 --gpu_ids 0 --part_to_swap makeup --swap_type style --att_loss
     
